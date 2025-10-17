07d61a178938799bea73e41ceaf435d54d3365f5
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2025-10-17 12:32:51 +0530

    Update button text to 'Visit My Portfolio & Projects'

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ return (
              focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 
              font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all"
 >
-  Contact me
+  Visit My Portfolio & Projects
 </button>
 
     </div>
